9984c46fffd33ed4e43ba506372c9c5472a3cd9c
plasante <pierre.lasante@videotron.ca>
2022-12-18 06:07:55 -0800

    Implemented OneToOne relationship between student and address tables.

--- src/main/java/com/uniksoft/entity/Student.java
+++ src/main/java/com/uniksoft/entity/Student.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -35,6 +37,10 @@ public class Student {
 	
 	@Column(name = "email")
 	private String email;
+	
+	@OneToOne
+	@JoinColumn(name = "address_id")
+	private Address address;
 	
 	@Transient
 	private String fullName;
