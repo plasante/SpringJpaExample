c64e41bf7aabd71908983d59a9e54fa285609c7b
plasante <pierre.lasante@videotron.ca>
2022-12-24 07:55:06 -0800

    Implemented @EnableScheduling

--- src/main/java/com/uniksoft/demo/DemoApplication.java
+++ src/main/java/com/uniksoft/demo/DemoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan({"com.uniksoft.controller", "com.uniksoft.service", "com.uniksoft.aop"})
 @EntityScan("com.uniksoft.entity")
 @EnableJpaRepositories("com.uniksoft.repository")
+@EnableScheduling
 //@EnableSwagger2
 public class DemoApplication {
 
