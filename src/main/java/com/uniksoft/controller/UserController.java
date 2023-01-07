c979d0d0f60213fd6b608f42eec2ef9506596a23
plasante <pierre.lasante@videotron.ca>
2023-01-07 07:29:44 -0800

    Heroku Adds UserController#sayHello

---
+++ src/main/java/com/uniksoft/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.uniksoft.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user/")
+public class UserController {
+
+    @GetMapping("/sayHello")
+    public String sayHello() {
+        return "Hello world from Rest Web Service...";
+    }
+}
